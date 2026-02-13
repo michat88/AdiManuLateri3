75e2ca26ffb5f77fc86bdbd30c7c3ad03ba43c8f
michat88 <gokilmichat@gmail.com>
2026-02-14 06:34:55 +0900

    Update Adicinemax21Plugin.kt

--- Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21Plugin.kt
+++ Adicinemax21/src/main/kotlin/com/Adicinemax21/Adicinemax21Plugin.kt
@@ -1,12 +1,13 @@
 package com.Adicinemax21
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin // <--- INI YANG PENTING
 import android.content.Context
 
 @CloudstreamPlugin
-class Adicinemax21Plugin: CloudstreamPlugin() {
+class Adicinemax21Plugin: Plugin() { // <--- Ubah "CloudstreamPlugin" jadi "Plugin"
     override fun load(context: Context) {
-        // Mendaftarkan Provider Utama kita
+        // Mendaftarkan Provider Utama
         registerMainAPI(Adicinemax21())
     }
 }
